f45027b54e578ab63952755308ede1fbe499a20c
nanggiomientrung <miracle.t084@gmail.com>
2019-12-28 08:44:21 +0700

    fist commit

---
+++ UnityGraph/Assets/Chart Feature/Scripts/Line Chart/LineRendererChart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererChart : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private LineRendererPointData nodeForDataPoint;
+
+    public void CreateLineChart(Color lineColor, Vector2[] listPoint)
+    {
+        lineRenderer.positionCount = listPoint.Length;
+        lineRenderer.startColor = lineColor;
+        lineRenderer.endColor = lineColor;
+
+        // đoạn này sẽ làm Bezier sau
+        for (int i = 0; i < listPoint.Length; i++)
+        {
+            lineRenderer.SetPosition(i, listPoint[i]);
+
+        }
+    }
+}
+
+public enum LineRendererType
+{
+    HorizontalAxis = 0,
+    VerticalAxis = 1,
+    LineRendererChart = 2
+}
